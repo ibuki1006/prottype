c71904c9d09f30e35cc8dc194d7bbdb9c766a510
Nacht0083 <ibukisuzuki1716@gmail.com>
2020-09-07 15:46:51 +0900

    でぃおんをでぃおに変更

--- include/top.php
+++ include/top.php
@@ -9,7 +9,7 @@
 							<h3>夜行バス</h3>
 						</div>
 						<div class="box-text">
-							<span>夜行バスでぃおん</span>
+							<span>夜行バスでぃお</span>
 						</div>
 						<div class="box-btn">
 						    <a href="index.php?page=detail">詳細</a>
