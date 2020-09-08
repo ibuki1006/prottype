a1ce6ba1de6f7fdd5ffb28d9cbc07161f99dae16
Nacht0083 <ibukisuzuki1716@gmail.com>
2020-09-08 16:07:38 +0900

    もどすよ

--- include/detail.php
+++ include/detail.php
@@ -3,52 +3,44 @@
 			<div class="container-fliud">
 				<div class="wrapper row">
 					<div class="preview col-md-6">
-
+						
 						<div class="preview-pic tab-content">
-						  <div class="tab-pane active" id="pic-1"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-2"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-3"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-4"><img src="http://placekitten.com/400/252" /></div>
-						  <div class="tab-pane" id="pic-5"><img src="http://placekitten.com/400/252" /></div>
+						  <div class="tab-pane active" id="pic-1"><i class="fas fa-bus fa-5x busicon"></i></div>
 						</div>
-						<ul class="preview-thumbnail nav nav-tabs">
-						  <li class="active"><a data-target="#pic-1" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-2" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-3" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-4" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						  <li><a data-target="#pic-5" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
-						</ul>
-
 					</div>
 					<div class="details col-md-6">
-						<h3 class="product-title">men's shoおおおおおおおes fashion</h3>
+						<h3 class="product-title">KB402 名古屋24:15集合（24：30出発）→横浜・東京《マスク・フェイスシールド無料配布》《換気機付き》</h3>
 						<div class="rating">
-							<div class="stars">
-								<span class="fa fa-star checked"></span>
-								<span class="fa fa-star checked"></span>
-								<span class="fa fa-star checked"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-							</div>
-							<span class="review-no">41 reviews</span>
+							<span class="review-no">残席：8/45</span>
 						</div>
-						<p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
-						<h4 class="price">current price: <span>$180</span></h4>
-						<p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
-						<h5 class="sizes">sizes:
+						<p class="product-description">
+						・当便運行経路:名古屋→横浜→東京<br>
+						・*マスク、フェイスシールド無料配布<br>
+						・*換気機付き<br>
+						・※最新安全性能システム搭載※<br>
+						・ひざ掛け無料貸出<br>
+						・各座席にUSB端子装備<br>
+						・(コンセントは装備されません)<br>
+						・＜名古屋でご乗車されるお客様へ＞<br>
+						・※表示している場所は集合場所であり、停留所ではありません。<br>
+						・※集合場所にて受付後、係員が停留所へご案内致します。<br>
+						・（停留所までは便により徒歩約5〜15分かかります。）<br>
+						</p>
+						<h4 class="price">値段：<span>3000円</span></h4>
+						<!-- <h5 class="sizes">sizes:
 							<span class="size" data-toggle="tooltip" title="small">s</span>
 							<span class="size" data-toggle="tooltip" title="medium">m</span>
 							<span class="size" data-toggle="tooltip" title="large">l</span>
 							<span class="size" data-toggle="tooltip" title="xtra large">xl</span>
-						</h5>
-						<h5 class="colors">colors:
+						</h5> -->
+						<!-- <h5 class="colors">colors:
 							<span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
 							<span class="color green"></span>
 							<span class="color blue"></span>
-						</h5>
+						</h5> -->
 						<div class="action">
-							<button class="add-to-cart btn btn-default" type="button">add to cart</button>
-							<button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button>
+							<button class="add-to-cart btn btn-primary" type="button">予約する</button>
+							<button class="like btn btn-primary" type="button"><span class="fa fa-heart fu"></span></button>
 						</div>
 					</div>
 				</div>
