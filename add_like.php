d893e65695b8208f90ee74aa55c98b2ddc15fb89
yamamoto <yamamoto.kazuki0715@gmail.com>
2020-11-02 10:52:41 +0900

    ちょっと編集

--- add_like.php
+++ add_like.php
@@ -1,4 +1,7 @@
 <?php
+  require_once("./DB/db_connect.php");
+  require_once("./class/ticket_class.php");
+
   $ticketNo = $_POST["ticketNo"];
-  echo $ticketNo."を受信したよ―――";
+  echo $ticketNo."を受信したよ";
  ?>
